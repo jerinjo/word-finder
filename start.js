1c436893e3cda5c8bed04dcfc880a3a4b805c19d
jerinjo <jerin.john2@cognizant.com>
2017-04-26 15:05:58 +0530

    hhh

--- start.js
+++ start.js
@@ -2,4 +2,12 @@ require('babel-register')({
   ignore: /(node_modules|dictionary\.js)/
 });
 
+
+var PythonShell = require('python-shell');
+
+PythonShell.run('my_script.py', function (err,results) {
+  if (err) throw err;
+  console.log('results: %j', results);
+});
+
 require('./server.js');
